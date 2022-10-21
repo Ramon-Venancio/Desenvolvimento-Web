ff5e50cf5447140dda3823272b136bf0697a0f6b
Ramon-Venacio <ramonvenanciosousa@gmail.com>
2022-10-21 16:46:16 -0300

    tabela e fonte

--- calculo-IMC/js/app.js
+++ calculo-IMC/js/app.js
@@ -2,6 +2,23 @@ function Calcular() {
     var altura = parseFloat(document.getElementById("altura").value);
     var peso = parseFloat(document.getElementById("peso").value);
 
-    document.getElementById("res").innerText = "Seu IMC:" + (peso / altura**2);  
+    imc = peso / altura**2
+    document.getElementById("res").innerText = "Seu IMC: " + imc;  
 
+
+    if (imc < 18.5) {
+        document.getElementById("imc").innerHTML = "Baixo do peso";
+    }
+    else if (imc < 24.9) {
+        document.getElementById("imc").innerHTML = "Peso normal";
+    }
+    else if (imc < 34.9) {
+        document.getElementById("imc").innerHTML = "Obesidade de Classe 1";
+    }
+    else if (imc < 39.9) {
+        document.getElementById("imc").innerHTML = "Obesidade de Classe 2";
+    }
+    else if (imc >= 40){
+        document.getElementById("imc").innerHTML = "Obesidade de Classe 3";
+    }
 }
